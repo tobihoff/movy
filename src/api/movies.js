0f780e44886b8895ee68f673e8610b07fbd49e62
tobihoff <lebenskraft.th@gmail.com>
2019-11-25 17:15:07 +0100

    add setTimeOut function

--- src/api/movies.js
+++ src/api/movies.js
@@ -3,5 +3,14 @@ export async function getDiscoverMovies(query) {
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${query}`
   );
   const data = await response.json();
+  await waitFor(500);
   return data.results;
+}
+
+function waitFor(milliseconds) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, milliseconds);
+  });
 }
